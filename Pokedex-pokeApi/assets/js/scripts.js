af599b9e38fe549dec90fbaf6cb1cd8faa202ff8
renawmontanari <regiangelica@gmail.com>
2022-08-22 14:45:40 -0300

    Consumindo API pokeApi

---
+++ Pokedex-pokeApi/assets/js/scripts.js
@@ -0,0 +1,33 @@
+
+'use strict'
+
+const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`;
+
+const generatePokemonPromises = () => Array(150).fill().map((_, index) => 
+    fetch(getPokemonUrl(index + 1)).then(response => response.json()))
+
+const generatorHTML = pokemons => pokemons.reduce((accumulator, { name, id, types }) => {
+    const elementTypes = types.map(typeInfo => typeInfo.type.name)
+
+    accumulator += `
+        <li class="card  ${elementTypes[0]}">
+        <img class="card-image" alt="${name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png" />
+            <h2 class="card-title">${id}. ${name}</h2>
+            <p class="card-subtitle">${elementTypes.join(' | ')}</p>
+        </li>
+    `
+    return accumulator
+}, '')
+
+
+const insertPokemonsIntoPage = pokemons => {
+    // Inserindo lis na página
+    const ul = document.querySelector('[data-js="pokedex"]')
+    ul.innerHTML = pokemons
+}
+
+const pokemonPromises = generatePokemonPromises()
+
+Promise.all(pokemonPromises)
+    .then(generatorHTML)
+    .then(insertPokemonsIntoPage)
